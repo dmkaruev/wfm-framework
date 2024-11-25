92d57c1245b16c98bc7759d367a215bdfb614321
dmkaruev <dmkaruev@mail.ru>
2024-11-26 00:38:45 +0300

    Правила роутов

--- public/index.php
+++ public/index.php
@@ -1,13 +1,15 @@
 <?php
 
-use FFI\Exception;
+use wfm\Router;
 
 if (PHP_MAJOR_VERSION < 8) {
     die('Необходима версия PHP >= 8');
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
 
-echo 'Hello';
+debug(Router::getRoutes());
